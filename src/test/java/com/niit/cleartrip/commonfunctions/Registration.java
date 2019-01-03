184a920bde370f02f68a432c72d70496a8110e95
Brahmachari <kajve01@ca.com>
2019-01-03 09:06:40 +0530

    New Methods are added

--- src/test/java/com/niit/cleartrip/commonfunctions/Registration.java
+++ src/test/java/com/niit/cleartrip/commonfunctions/Registration.java
@@ -28,5 +28,20 @@ WebDriverInitialization webdriverintraction;
 		webdriverintraction.click("com.cleartrip.flights.userAccount.register.signUp");
 		
 	}
+	public void failureRegistrations(){
+		webdriverintraction.click("com.cleartrip.flights.userAccount");
+		webdriverintraction.click("com.cleartrip.flights.userAccount.register");
+		webdriverintraction.implicitwait(10);
+		webdriverintraction.SWITCHTOFRAME_BYUSING_NAME("modal_window");
+		webdriverintraction.SENDKEYS("com.cleartrip.flights.userAccount.register.email","com.cleartrip.flights.userAccount.emailValue");
+		webdriverintraction.click("com.cleartrip.flights.userAccount.register.submit");
+		webdriverintraction.implicitwait(10);
+		webdriverintraction.SENDKEYS("com.cleartrip.flights.userAccount.register.password","com.cleartrip.flights.userAccount.register.passwordValue");
+		webdriverintraction.SELECTBYVALUE("com.cleartrip.flights.userAccount.register.title","com.cleartrip.flights.userAccount.register.titleValue");
+		webdriverintraction.SENDKEYS("com.cleartrip.flights.userAccount.register.firstname","com.cleartrip.flights.userAccount.register.firstnameValue");
+		webdriverintraction.SENDKEYS("com.cleartrip.flights.userAccount.register.lastname","com.cleartrip.flights.userAccount.register.lastnameValue");
+		webdriverintraction.SENDKEYS("com.cleartrip.flights.userAccount.register.mobilenumber","com.cleartrip.flights.userAccount.register.mobilenumberValue");
+		webdriverintraction.click("com.cleartrip.flights.userAccount.register.signUp");
+	}
 
 }
