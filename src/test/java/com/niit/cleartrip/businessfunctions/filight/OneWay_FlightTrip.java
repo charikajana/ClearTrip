2b915121f9190d88beeeab90ae6cab65a9ec8a73
Venu 9955 <teepireddy@gmail.com>
2019-01-05 13:35:06 -0800

    code added by venu

--- src/test/java/com/niit/cleartrip/businessfunctions/filight/OneWay_FlightTrip.java
+++ src/test/java/com/niit/cleartrip/businessfunctions/filight/OneWay_FlightTrip.java
@@ -1,5 +1,13 @@
 package com.niit.cleartrip.businessfunctions.filight;
 
+import org.openqa.selenium.WebDriver;
+
 public class OneWay_FlightTrip {
+	
+	public WebDriver driver;
+	
+	public static void main(String[] args) {
+		
+	}
 
 }
